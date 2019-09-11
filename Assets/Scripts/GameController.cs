9a972ebe2f8aa546ce6e2542556b5da09663ac2b
CreatorRedA <creatorreda@gmail.com>
2019-09-11 11:44:11 -0400

    Original project commit. Print "It's been 3 seconds" to console.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    int notifications = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Time.time) >= 3 && notifications == 0)
+        {
+            print((Time.time) + " It's been three seconds!");
+            notifications++;
+        }
+
+    }
+}
