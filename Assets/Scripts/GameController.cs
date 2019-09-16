0cbb37a0095f3b894de0d6dedd8eeb04dfcad4ba
CreatorRedA <creatorreda@gmail.com>
2019-09-16 11:51:53 -0400

    Mining Project Part 2

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    int notifications = 0;
+    int notifications;
+    float miningSpeed;
+    int bronzeSupply;
+    int bronze;
+    int silverSupply;
+    int silver;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        miningSpeed = 3;
+        bronzeSupply = 3;
+        silverSupply = 2;
+        bronze = 0;
+        silver = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Time.time) >= 3 && notifications == 0)
+        //If available, mine bronze
+        if ((Time.time) >= miningSpeed && bronzeSupply > 0)
         {
-            print((Time.time) + " It's been three seconds!");
-            notifications++;
+            bronzeSupply--;
+            bronze++;
+            miningSpeed += 3; //Keeps miningSpeed in time with Time.time
+            print("Time.time = " + (Time.time) + "\nYour bronze: " + bronze + " AND Your silver: " + silver);
+        }
+
+        //If availabe, mine silver
+        else if ((Time.time) >= miningSpeed && silverSupply > 0)
+        {
+            silverSupply--;
+            silver++;
+            miningSpeed += 3;
+            print("Time.time = " + (Time.time) + "\nYour bronze: " + bronze + " AND Your silver: " + silver + "\n Time.time = " + (Time.time));
         }
 
     }
